4563d346fa8805b40e87d1f8641a28aedad770cf
Vigneshwar485 <vigneshwarreddy1101@gmail.com>
2026-04-15 10:17:10 +0530

    UC2:Feet and Inches measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,5 +1,8 @@
+package com.apps.quantitymeasurement;
+
 public class QuantityMeasurementApp {
 
+    // Feet class
     public static class Feet {
         private final double value;
 
@@ -9,12 +12,9 @@ public class QuantityMeasurementApp {
 
         @Override
         public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
@@ -25,10 +25,37 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
-        Feet value1 = new Feet(1.0);
-        Feet value2 = new Feet(1.0);
+    // Inches class
+    public static class Inches {
+        private final double value;
 
-        System.out.println("Are values equal? " + value1.equals(value2));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Feet Equal: " + f1.equals(f2));
+        System.out.println("Inches Equal: " + i1.equals(i2));
     }
 }
