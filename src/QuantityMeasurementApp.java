9946c6c37bb12962bd66e60f13370d5149d0cff6
Vigneshwar485 <vigneshwarreddy1101@gmail.com>
2026-04-15 10:18:54 +0530

    UC3: Generic Quantity Class for DRY Principle

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,36 +1,49 @@
 package com.apps.quantitymeasurement;
 
+import java.util.Objects;
+
 public class QuantityMeasurementApp {
 
-    // Feet class
-    public static class Feet {
-        private final double value;
+    // Enum for units
+    public enum LengthUnit {
+        FEET(12.0),
+        INCHES(1.0);
 
-        public Feet(double value) {
-            this.value = value;
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
+        public double getConversionFactor() {
+            return conversionFactor;
         }
     }
 
-    // Inches class
-    public static class Inches {
+    // Generic Length class (merged here)
+    public static class Length {
         private final double value;
+        private final LengthUnit unit;
 
-        public Inches(double value) {
+        public Length(double value, LengthUnit unit) {
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double convertToBaseUnit() {
+            return value * unit.getConversionFactor();
+        }
+
+        public boolean compare(Length other) {
+            if (other == null) return false;
+
+            return Double.compare(
+                    this.convertToBaseUnit(),
+                    other.convertToBaseUnit()
+            ) == 0;
         }
 
         @Override
@@ -38,24 +51,46 @@ public class QuantityMeasurementApp {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
 
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
+            Length other = (Length) obj;
+            return compare(other);
         }
 
         @Override
         public int hashCode() {
-            return Double.hashCode(value);
+            return Objects.hash(convertToBaseUnit());
         }
+    }
+
+    // Methods for demo
+
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+        return l1.equals(l2);
+    }
+
+    public static void demonstrateFeetEquality() {
+        Length f1 = new Length(1.0, LengthUnit.FEET);
+        Length f2 = new Length(1.0, LengthUnit.FEET);
+
+        System.out.println("Feet equal: " + demonstrateLengthEquality(f1, f2));
+    }
+
+    public static void demonstrateInchesEquality() {
+        Length i1 = new Length(1.0, LengthUnit.INCHES);
+        Length i2 = new Length(1.0, LengthUnit.INCHES);
+
+        System.out.println("Inches equal: " + demonstrateLengthEquality(i1, i2));
+    }
+
+    public static void demonstrateFeetInchesComparison() {
+        Length f = new Length(1.0, LengthUnit.FEET);
+        Length i = new Length(12.0, LengthUnit.INCHES);
+
+        System.out.println("Feet == Inches: " + demonstrateLengthEquality(f, i));
     }
 
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-
-        Inches i1 = new Inches(1.0);
-        Inches i2 = new Inches(1.0);
-
-        System.out.println("Feet Equal: " + f1.equals(f2));
-        System.out.println("Inches Equal: " + i1.equals(i2));
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
+        demonstrateFeetInchesComparison();
     }
 }
